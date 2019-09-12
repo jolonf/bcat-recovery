f7b772722ffeb731a321acb4a20fef7604a41416
Jolon <22928840+jolonf@users.noreply.github.com>
2019-09-13 08:35:46 +1000

    Initial commit

---
+++ bcat-recovery.js
@@ -0,0 +1,68 @@
+
+const button = document.querySelector('#prepareButton');
+
+button.addEventListener('click', prepareTransaction);
+
+function prepareTransaction(e) {
+
+  e.preventDefault();
+
+  const fileName = document.querySelector('#fileName').value || ' ';
+  const mimeType = document.querySelector('#mimeType').value || ' ';
+  const encoding = document.querySelector('#encoding').value || ' ';
+  const txIdTextArea = document.querySelector('#txIds').value;
+
+  const txIds = txIdTextArea.match(/\S+/g);
+
+  if (!txIds) {
+    alert(`No transaction ids entered`);
+    return;
+  }
+
+  for (const txId of txIds) {
+    if (!/[0-9A-Fa-f]{64}/g.test(txId)) {
+      alert(`Not a valid transaction id: ${txId}`);
+      return;
+    }
+  }
+
+  const script = [
+    '15DHFxWZJT58f9nhyGnsRBqrgwK4W6h4Up',
+    ' ',
+    mimeType,
+    encoding,
+    fileName,
+    ' ',
+    ...txIds.map(txId => bsv.deps.Buffer.from(txId, 'hex'))
+  ]
+
+  const asm = bsv.Script.buildSafeDataOut(script).toASM();
+
+  const moneyButtonDiv = document.querySelector('#bcat-money-button');
+
+  moneyButton.render(moneyButtonDiv, {
+    label: "Send",
+    clientIdentifier: "3fb24dea420791729b4d9b39703c6339",
+    type: "buy",
+    outputs: [{ 
+      script: asm, 
+      amount: 0, 
+      currency: 'BSV' 
+    }],
+    onPayment: onPayment,
+    onError: onError
+  });
+
+}
+
+function onPayment(arg) {
+  console.log(`Payment successful, txid: ${arg.txid}`);
+  const pTxId = document.querySelector('#txId');
+
+  pTxId.innerHTML = `<a href="https://bico.media/${arg.txid}" target='_blank'>${arg.txid}</a>`;
+}
+
+function onError(arg) {
+  console.log('Payment error', arg);
+  alert('Payment error');
+}
